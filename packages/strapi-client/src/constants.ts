0c330c01c232947af8785ff37520e11d960a5c40
Richard Weaver <r.weaver@enotriacoe.com>
2023-06-04 16:25:45 +0100

    chore: add jsdoc comments to constants

--- packages/strapi-client/src/constants.ts
+++ packages/strapi-client/src/constants.ts
@@ -1,5 +1,8 @@
 import { StrapiClientOptions } from './types';
 
+/**
+ * The default options to use for the StrapiClient.
+ */
 export const defaultOptions: StrapiClientOptions = {
   url: 'http://127.0.0.1:1337',
   prefix: '/api',
@@ -8,7 +11,14 @@ export const defaultOptions: StrapiClientOptions = {
   axiosConfig: {},
 };
 
+/**
+ * The regex used to identify ISO date strings.
+ */
 export const ISO_DATE_REGEX =
   /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/;
 
+/**
+ * The default regex used to identify date properties.
+ * (Property names ending in 'at', 'on' or 'date', case insensitive)
+ */
 export const DATE_PROPERTY_REGEX = /^.+(at|on|date)$/i;
